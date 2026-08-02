b73f35148b98f33d33d33e7b2390b72722955547
Sayan <sheesayan2001@gmail.com>
2026-08-02 12:18:45 +0530

    Load DagsHub token from AWS Parameter Store

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -1,30 +1,3 @@
-# #!/bin/bash
-# # Log everything to start_docker.log
-# exec > /home/ubuntu/start_docker.log 2>&1
-
-# echo "Logging in to ECR..."
-# aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 891377050051.dkr.ecr.ap-south-1.amazonaws.com
-
-# echo "Pulling Docker image..."
-# docker pull 891377050051.dkr.ecr.ap-south-1.amazonaws.com/food_delivery_time_prediction:latest
-
-# echo "Checking for existing container..."
-# if [ "$(docker ps -q -f name=delivery_time_pred)" ]; then
-#     echo "Stopping existing container..."
-#     docker stop delivery_time_pred
-# fi
-
-# if [ "$(docker ps -aq -f name=delivery_time_pred)" ]; then
-#     echo "Removing existing container..."
-#     docker rm delivery_time_pred
-# fi
-
-# echo "Starting new container..."
-# docker run -d -p 80:8000 --name delivery_time_pred -e DAGSHUB_USER_TOKEN=0b44756fc3f18f453afbddad59dec563ff9e691c 891377050051.dkr.ecr.ap-south-1.amazonaws.com/food_delivery_time_prediction:latest
-
-# echo "Container started successfully." 
-
-
 #!/bin/bash
 set -e
 
@@ -35,6 +8,15 @@ AWS_ACCOUNT_ID="241077340105"
 ECR_REPOSITORY="swiggy-delivery-time-prediction"
 
 IMAGE="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPOSITORY}:latest"
+
+echo "Getting DagsHub token from Parameter Store..."
+
+DAGSHUB_USER_TOKEN=$(aws ssm get-parameter \
+  --name "/swiggy/dagshub-token" \
+  --with-decryption \
+  --region us-east-1 \
+  --query "Parameter.Value" \
+  --output text)
 
 echo "Logging in to ECR..."
 
@@ -51,10 +33,13 @@ echo "Removing existing container..."
 docker rm -f delivery_time_pred 2>/dev/null || true
 
 echo "Starting new container..."
+
 docker run -d \
   --name delivery_time_pred \
   -p 8000:8000 \
+  -e DAGSHUB_USER_TOKEN="$DAGSHUB_USER_TOKEN" \
   "$IMAGE"
 
 echo "Container started successfully"
+
 docker ps
